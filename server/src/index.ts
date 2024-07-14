875a54c5220dedd861dbaf52f935ee9d6a8498fa
hemant <hk443957@gmail.com>
2024-07-14 23:03:53 +0530

    project running successfully

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ AppDataSource.initialize().then(() => {
 
     app.use("/api", userRoutes);
 
-    app.listen(8080, () => {
-        console.log("Server is running on port 3000");
+    app.listen(8082, () => {
+        console.log("Server is running on port 8082");
     });
 }).catch((error) => console.log(error));
